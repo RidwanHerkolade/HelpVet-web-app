1e3347b6cdd2b923c6c326ea6c7153395d0accfc
Lasisi Ridwan Akolade <ridwanlasisi3@gmail.com>
2022-09-21 04:52:10 +0100

    react-commit

--- src/component/Header/Header.jsx
+++ src/component/Header/Header.jsx
@@ -8,6 +8,7 @@ import { Link } from "react-router-dom";
 
 
 
+
 function Header(){
      const [mobile, setMobile]= useState(false)
      function handleClicks(){
--- src/component/Section/Section.jsx
+++ src/component/Section/Section.jsx
@@ -1,5 +1,7 @@
 import React from "react";
 import './Section.css'
+import { Link } from "react-router-dom";
+import ArrowForwardIcon from '@mui/icons-material/ArrowForward';
 
 function Section(props){
      return(
@@ -8,7 +10,9 @@ function Section(props){
                      <div className="section__content">
                              <h2>Get <span className="span1">maximum</span> and <br/> <span className="span2">affordable care</span> for<br/> your pets</h2>
                               <p>Take care of your pet,never leave them sick and weak .we are here to help you treat your pets and help them grow.</p>
-                              <div className="book">Book appointment<span class="material-symbols-sharp">arrow_forward</span></div>
+                              <div className="book"><Link to="/register" className="book__link">Book appointment<ArrowForwardIcon/></Link></div>
+                           
+                        
                      
                      
                      </div>
